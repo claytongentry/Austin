8f0b4858cce2c4e72ca81523d17d591bba370827
cg3ntry <cg3ntry@gmail.com>
2015-03-31 13:52:56 -0500

    Preliminary template

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php require('templates/header.php'); ?>
 
-  <h1>Excuse me!</h1>
-  <p>Patience is a virtue</p>
-  
+  <!--Big pretty fixed splash page here-->
+
+  <!--Nav bar w/ About, CV, Contact - would suggest having it start below the splash page, then maybe stick to the top of the screen on scroll-->
+
+  <!--"Skills" grid goes here - directing, editing, cinematography, motion graphics, whatever else-->
+
+  <!--Footer - copyright, date, Facebook, email link perhaps-->
+
 <?php require('templates/footer.php'); ?>
