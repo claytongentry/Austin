965a38b5354a639060880b8d279cc8438e92b823
cg3ntry <cg3ntry@gmail.com>
2015-04-03 16:24:49 -0500

    Skeleton html

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <?php require('templates/header.php'); ?>
 
   <!--Big pretty fixed splash page here-->
+  <div id="splash">
+      <h1>Austin Curzon</h1>
+      <p>Patience is a virtue.</p>
+  </div>
 
   <!--Nav bar w/ About, CV, Contact - would suggest having it start below the splash page, then maybe stick to the top of the screen on scroll-->
+  <ul id="nav">
+      <li><a href="#">About</a></li>
+      <li><a href="#">Resume</a></li>
+      <li><a href="#">Contact</a></li>
+  </ul>
 
   <!--"Skills" grid goes here - directing, editing, cinematography, motion graphics, whatever else-->
+  <table>
+      <tr>
+          <td>Directing</td>
+          <td>Cinematograpy</td>
+      </tr>
+      <tr>
+          <td>Motion Graphics</td>
+          <td>Editing</td>
+      </tr>
+  </table>
 
   <!--Footer - copyright, date, Facebook, email link perhaps-->
+  <div id="footer">
+      <p>Site Production &copy; Clayton Gentry 2015</p>
+      <ul>
+        <li><a target="_blank" href="https://www.facebook.com/austin.curzon">Facebook</a></li>
+        <li><a href="mailto:awcurzon@gmail.com">Email</a></li>
+      </ul>
+  </div>
 
 <?php require('templates/footer.php'); ?>
--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
   <html lang = "en">
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=1">
+      <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
       <link type="text/css" rel="stylesheet" href="styles/styles.css"/>
       <title>Austin Curzon</title>
     </head>
