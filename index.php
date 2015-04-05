5d599a450ca23681cac48b418950e2a4c3d3359c
cg3ntry <cg3ntry@gmail.com>
2015-04-05 12:54:23 -0500

    Skeleton

--- index.php
+++ index.php
@@ -7,23 +7,25 @@
   </div>
 
   <!--Nav bar w/ About, CV, Contact - would suggest having it start below the splash page, then maybe stick to the top of the screen on scroll-->
-  <ul id="nav">
+  <!--<ul id="nav">
       <li><a href="#">About</a></li>
       <li><a href="#">Resume</a></li>
       <li><a href="#">Contact</a></li>
-  </ul>
+  </ul>-->
 
   <!--"Skills" grid goes here - directing, editing, cinematography, motion graphics, whatever else-->
-  <table>
-      <tr>
-          <td><h3>Directing</h3></td>
-          <td><h3>Cinematograpy</h3></td>
-      </tr>
-      <tr>
-          <td><h3>Motion Graphics</h3></td>
-          <td><h3>Editing</h3></td>
-      </tr>
-  </table>
+  <div id="skills">
+      <table>
+          <tr>
+              <td><h3>Directing</h3></td>
+              <td><h3>Cinematograpy</h3></td>
+          </tr>
+          <tr>
+              <td><h3>Motion Graphics</h3></td>
+              <td><h3>Editing</h3></td>
+          </tr>
+      </table>
+  </div>
 
   <!--Footer - copyright, date, Facebook, email link perhaps-->
   <div id="footer">
