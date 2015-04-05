8150c69cb80865707ef27eb05effd03dc9435740
cg3ntry <cg3ntry@gmail.com>
2015-04-05 16:20:17 -0500

    Splash draft

--- index.php
+++ index.php
@@ -1,33 +1,49 @@
 <?php require('templates/header.php'); ?>
 
-  <!--Big pretty fixed splash page here-->
-  <div id="splash">
-      <h1>Austin Curzon</h1>
-      <p>Patience is a virtue.</p>
-  </div>
-
   <!--Nav bar w/ About, CV, Contact - would suggest having it start below the splash page, then maybe stick to the top of the screen on scroll-->
-  <!--<ul id="nav">
+  <ul id="nav">
       <li><a href="#">About</a></li>
       <li><a href="#">Resume</a></li>
       <li><a href="#">Contact</a></li>
-  </ul>-->
+  </ul>
 
-  <!--"Skills" grid goes here - directing, editing, cinematography, motion graphics, whatever else-->
+  <!--Big pretty fixed splash page here-->
+  <div id="splash">
+      <h1>Austin Curzon</h1>
+      <p>Filmmaker &bull; After Effects Pro &bull; Damn Good Pal</p>
+      <a href="#">
+          <div id="reel">
+              <p>Reel</p>
+          </div>
+      </a>
+      <i class="fa fa-chevron-down fa-5x"></i>
+  </div>
+
+  <!--
+  "Skills" grid goes here - directing, editing, cinematography, motion graphics, whatever else
   <div id="skills">
+      <h2>My Skills</h2>
       <table>
           <tr>
-              <td><h3>Directing</h3></td>
-              <td><h3>Cinematograpy</h3></td>
+              <td>
+                  <h3>Directing</h3>
+              </td>
+              <td>
+                  <h3>Cinematography</h3>
+              </td>
           </tr>
           <tr>
-              <td><h3>Motion Graphics</h3></td>
-              <td><h3>Editing</h3></td>
+              <td>
+                  <h3>Motion Graphics</h3>
+              </td>
+              <td>
+                  <h3>Editing</h3>
+              </td>
           </tr>
       </table>
   </div>
 
-  <!--Footer - copyright, date, Facebook, email link perhaps-->
+  Footer - copyright, date, Facebook, email link perhaps
   <div id="footer">
       <p>Site Production &copy; Clayton Gentry 2015</p>
       <ul>
@@ -35,5 +51,6 @@
         <li><a href="mailto:awcurzon@gmail.com">Email</a></li>
       </ul>
   </div>
+  -->
 
 <?php require('templates/footer.php'); ?>
--- templates/header.php
+++ templates/header.php
@@ -2,8 +2,9 @@
   <html lang = "en">
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=1">
-      <link href="http://fonts.googleapis.com/css?family=Lekton" rel="stylesheet" type="text/css">
-      <link href="http://fonts.googleapis.com/css?family=Molengo" rel="stylesheet" type="text/css">
+      <link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css">
+      <link href='http://fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
       <link type="text/css" rel="stylesheet" href="styles/styles.css"/>
       <title>Austin Curzon</title>
     </head>
