0e8abc6cc7f20b92a81eec3c5114e3133674ba85
cg3ntry <cg3ntry@gmail.com>
2015-04-22 13:58:16 -0500

    Vimeo sticker updated

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -2,7 +2,7 @@
         <div id="footer">
             <ul>
               <li><a target="_blank" href="https://www.facebook.com/austin.curzon"><i class="fa fa-facebook fa-2x"></i></a></li>
-              <li><a target = "_blank" href = "https://vimeo.com/user13947441"><i class="fa fa-vimeo-square fa-2x"></i></a></li>
+              <li><a target = "_blank" href = "https://vimeo.com/austincurzon"><i class="fa fa-vimeo-square fa-2x"></i></a></li>
               <li><a href="mailto:awcurzon@gmail.com"><i class="fa fa-envelope fa-2x"></i></a></li>
             </ul>
             <p>Development &copy; Clayton Gentry 2015</p>
