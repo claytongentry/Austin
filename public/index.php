064e7655b05b0d15a6f38c89d8135c5611ad0953
cg3ntry <cg3ntry@gmail.com>
2015-04-22 15:18:48 -0500

    Grid and video enlarged

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
       <h1>Austin Curzon</h1>
       <p>Filmmaker &bull; After Effects Pro &bull; Damn Good Pal</p>
       <a href="#"> <!--Vimeo link-->
-          <div id="reel">
+          <div class = "btn" id="reel">
               <p>Reel</p>
           </div>
       </a>
@@ -73,7 +73,7 @@
           <input type = "text" name = "name" id = "name" placeholder = "Name"></input>
           <input type = "text" name = "email" id = "email" placeholder = "Email"></input><br/>
           <textarea id = "msg" rows = "6" cols = "60" placeholder = "Your message here..."></textarea><br/>
-          <input type = "submit" class = "btn" value = "Send" style = "padding: 0px;"></input>
+          <input id = "submit_btn" type = "submit" class = "btn" value = "Send"></input>
       </form>
   </div>
 <?php require('templates/footer.php'); ?>
