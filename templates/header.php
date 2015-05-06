7a8c3517050a8148b612149c015bf135d073ef7b
cg3ntry <cg3ntry@gmail.com>
2015-05-06 10:10:17 -0500

    Fonts load over secureconnection

--- templates/header.php
+++ templates/header.php
@@ -3,10 +3,8 @@
       <head>
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=1">
-          <link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
-          <link href='https://fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
+          <link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
           <link href='https://fonts.googleapis.com/css?family=Raleway:900' rel='stylesheet' type='text/css'>
-          <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400' rel='stylesheet' type='text/css'>          <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
           <link type = "image/x-icon" rel = "shortcut icon" href = "favicon.ico"/>
           <link type="text/css" rel="stylesheet" href="styles/styles.css"/>
           <title>Austin Curzon</title>
